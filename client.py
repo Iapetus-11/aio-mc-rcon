83a2dd6b3db4f5d66d3fa9be13208b16e551a7cd
Iapetus-11 <olimdahacker@gmail.com>
2020-11-06 11:57:49 -0500

    add basic client and functions

---
+++ client.py
@@ -0,0 +1,51 @@
+import asyncio
+import struct
+
+
+class RCONClient:
+    def __init__(self, host, auth):
+        split = host.split(':')
+
+        self.host = split[0]
+        self.port = int(split[1]) if len(split) > 1 else 25575
+
+        self.auth = auth
+
+        self._reader = None
+        self._writer = None
+
+        asyncio.run(self._setup())
+
+    async def _setup(self):
+        self._reader, self._writer = await asyncio.open_connection(self.host, self.port)
+        await self._send(3, self.auth)
+
+    async def _read(self, n_bytes):
+        data = b''
+
+        while len(data) < n_bytes:
+            data += await self._reader.read(n_bytes - len(data))
+
+        return data
+
+    async def _send(self, _type, msg):  # for _types: 3=login/authenticate, 2=command, 0=cmd response, -1=invalid auth
+        out_msg = struct.pack('<li', 0, _type) + msg.encode('utf8') + b'\x00\x00'
+        out_len = struct.pack('<i', len(out_msg))
+        self._writer.write(out_len + out_msg)
+
+        in_len = struct.unpack('<i', await self._read(4))
+        in_msg = await self._read(in_len[0])
+
+        in_id = struct.unpack('<ii', in_msg[:8])[0]
+        in_data, in_pad = in_msg[8:-2], in_msg[-2:]
+
+        if in_id == -1: raise Exception('Invalid authentication')
+
+        return in_data.decode('utf8')
+
+    async def send_command(self, msg):
+        return await self._send(2, msg)
+
+    async def close(self):
+        self._writer.close()
+        await self._writer.wait_closed()
