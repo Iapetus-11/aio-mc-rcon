54faa0a620d2cc26fb49a7cf4da17a08cca21709
Iapetus-11 <olimdahacker@gmail.com>
2020-11-06 13:22:58 -0500

    attempt to fix PacketTypes class

--- aiomcrcon/Types.py
+++ aiomcrcon/Types.py
@@ -2,7 +2,7 @@
 class PacketTypes:
     """Packet types that can be sent or received"""
 
-    self.LOGIN = 3
-    self.COMMAND = 2
-    self.COMMAND_RESPONSE = 0
-    self.INVALID_AUTH = -1
+    LOGIN = 3
+    COMMAND = 2
+    COMMAND_RESPONSE = 0
+    INVALID_AUTH = -1
