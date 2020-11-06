7e89647e3c0998e7d3a323985fb97d71ade00f74
Iapetus-11 <olimdahacker@gmail.com>
2020-11-06 12:38:27 -0500

    add custom exceptions

---
+++ aio-mc-rcon/Errors.py
@@ -0,0 +1,23 @@
+
+class TimeoutError(Exception):
+    """Raised when a timeout occurs"""
+
+    def __init__(self, reason='TimeoutError'):
+        self.reason = reason
+
+    def __str__(self):
+        return self.reason
+
+
+class InvalidAuth(Exception):
+    """Raised when the password is incorrect"""
+
+    def __str__(self):
+        return 'The provided authentication/password was invalid'
+
+
+class InvalidDataReceivedError(Exception):
+    """Raised when the client receives invalid data from the server"""
+
+    def __str__(self):
+        return 'Invalid data was received from the server'
