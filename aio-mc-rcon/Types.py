deb62521e01956ccfe7d6770f24b890f81d67b41
Iapetus-11 <olimdahacker@gmail.com>
2020-11-06 12:43:01 -0500

    make PacketTypes a class again

--- aio-mc-rcon/Types.py
+++ aio-mc-rcon/Types.py
@@ -1,6 +1,9 @@
 
-"""Packet types that can be sent or received"""
-self.LOGIN = 3
-self.COMMAND = 2
-self.COMMAND_RESPONSE = 0
-self.INVALID_AUTH = -1
+class PacketTypes:
+    """Packet types that can be sent or received"""
+
+    def __init__(self):
+        self.LOGIN = 3
+        self.COMMAND = 2
+        self.COMMAND_RESPONSE = 0
+        self.INVALID_AUTH = -1
