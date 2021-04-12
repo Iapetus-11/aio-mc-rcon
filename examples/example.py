a2208ccd03e1be4ec911214339193d6ca281a927
Milo Weinberg <iapetus011@gmail.com>
2021-04-11 22:13:49 -0400

    add another example

---
+++ examples/example.py
@@ -0,0 +1,18 @@
+from aiomcrcon import Client
+import asyncio
+
+
+async def main():
+    password = input("Password? ")
+    command = input("Command? ")
+
+    client = Client("xenonmc.ml", 25566, password)
+    await client.connect()
+
+    response = await client.send_cmd(command)
+    print(response)
+
+    await client.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
