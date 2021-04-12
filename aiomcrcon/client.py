19a6ba4b4d20e471fac557ee05bd178535dd5fc3
Milo Weinberg <iapetus011@gmail.com>
2021-04-11 21:24:24 -0400

    add send_cmd method

--- aiomcrcon/client.py
+++ aiomcrcon/client.py
@@ -1,7 +1,15 @@
 import asyncio
+import random
+import struct
 
-from .errors import RCONConnectionError
+from .errors import RCONConnectionError, IncorrectPasswordError
 
+
+class MessageType:
+    LOGIN = 3
+    COMMAND = 2
+    RESPONSE = 0
+    INVALID_AUTH = -1
 
 class Client:
     """The base class for creating an RCON client."""
@@ -32,7 +40,44 @@ class Client:
         except Exception as e:
             raise RCONConnectionError("The connection failed for an unknown reason.", e)
 
-    async def _send(self, type_: int, msg: str) -> tuple:
-        """Sends data to the server."""
+    async def _send_msg(self, type_: int, msg: str) -> tuple:
+        """Sends data to the server, and returns the response."""
 
-        raise NotImplementedError
+        if not self._ready:
+            raise ClientNotConnectedError
+
+        # randomly generate request id
+        req_id = random.randint(0, 2147483647)
+
+        # pack request id, packet type, and the actual message
+        packet_data = struct.pack("<ii", req_id, type_) + msg.encode("utf8") + b"\x00\x00"
+
+        # pack length of packet + rest of packet data
+        packet = struct.pack("<i", len(packet_data)) + packet_data
+
+        # send the data to the server
+        await self._writer.write(packet)
+        await self._writer.drain()
+
+        # read + unpack length of incoming packet
+        in_len = struct.unpack("<i", *(await self._reader.read(4))
+
+        # read rest of packet data
+        in_data = await self._reader.read(in_len)
+
+        if not in_data.endswith(b"\x00\x00"):
+            raise ValueError("Invalid data received from server.")
+
+        # decode the incoming request id and packet type
+        in_req_id, in_type = struct.unpack("<ii", in_data[0:8])
+
+        if in_type == MessageType.INVALID_AUTH:
+            raise IncorrectPasswordError
+
+        # decode the received message
+        in_msg = in_data[8:-2].decode("utf8")
+
+        return in_msg, in_type
+
+    async def send_cmd(self, cmd: str) -> tuple:
+        return await self._send_msg(Messagetype.COMMAND, cmd)
