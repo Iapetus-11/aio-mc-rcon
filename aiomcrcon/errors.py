d17e805bc5eb319c7fc97fa5d95439a2cb6cb750
Milo Weinberg <iapetus011@gmail.com>
2021-04-11 21:21:10 -0400

    add ClientNotConnectedError and IncorrectPasswordError exceptions

--- aiomcrcon/errors.py
+++ aiomcrcon/errors.py
@@ -6,3 +6,16 @@ class RCONConnectionError(Exception):
 
         self.message = msg
         self.error = error
+
+class ClientNotConnectedError(Exception):
+    """Raised when an IO method is used when the Client isn't connected."""
+
+    def __str__(self):
+        return "The client isn't connected. (Looks like you forgot to call the connect() coroutine!)"
+
+
+class IncorrectPasswordError(Exception):
+    """Raised when the RCON authentication / password is incorrect."""
+
+    def __str__(self):
+        return "The password provided to the client was incorect according to the server."
