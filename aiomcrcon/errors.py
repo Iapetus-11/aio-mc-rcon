7b6225866717e32dce8a454fa7a342cc352af066
Milo Weinberg <iapetus011@gmail.com>
2021-04-11 20:50:24 -0400

    add RCONConnectionError

---
+++ aiomcrcon/errors.py
@@ -0,0 +1,7 @@
+
+class RCONConnectionError(Exception):
+    def __init__(self, msg: str = None, error: Exception = None):
+        super().__init__(msg)
+
+        self.message = msg
+        self.error = error
