623b571bf721246856e5bdbf5ff48d26e75af55e
Milo Weinberg <iapetus011@gmail.com>
2023-12-30 15:09:01 -0500

    Remove `__version__` constant using deprecated api in py 3.12

--- aiomcrcon/__init__.py
+++ aiomcrcon/__init__.py
@@ -1,4 +1,2 @@
 from .client import MessageType, Client
 from .errors import RCONConnectionError, ClientNotConnectedError, IncorrectPasswordError
-
-__version__: str = __import__("pkg_resources").get_distribution("aio-mc-rcon").version
