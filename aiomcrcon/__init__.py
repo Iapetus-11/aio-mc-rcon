e49e3e789057e0ca2fef32dffb65f2bf5dc2b6a7
Milo Weinberg <iapetus011@gmail.com>
2025-01-16 21:40:16 -0500

    Modernize CI + Upgrade Dependencies + Drop <Py3.9 Support + Misc Improvements (#9)

--- aiomcrcon/__init__.py
+++ aiomcrcon/__init__.py
@@ -1,2 +1,14 @@
+import importlib
 from .client import MessageType, Client
 from .errors import RCONConnectionError, ClientNotConnectedError, IncorrectPasswordError
+
+__version__ = importlib.metadata.version("aio-mc-rcon")
+
+__all__ = (
+    "MessageType",
+    "Client",
+    "RCONConnectionError",
+    "ClientNotConnectedError",
+    "IncorrectPasswordError",
+    "__version__",
+)
